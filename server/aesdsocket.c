a1abf5031d950cb6734a64a40f49012674b47fe4
celeseclipse <hakunamatata5515@gmail.com>
2025-05-06 14:32:14 +0000

    [Assignment 6 part 1] multithread socket stream

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -11,187 +11,188 @@
 #include <errno.h>
 #include <sys/stat.h>
 #include <getopt.h>
+#include <sys/queue.h>
+#include <pthread.h>
+#include <stdbool.h>
+
+// Threaded client struct
+struct client {
+    int client_fd;
+    pthread_t thread;
+    bool complete;
+    SLIST_ENTRY(client) entries;
+};
+
+SLIST_HEAD(client_list, client);
+struct client_list client_head = SLIST_HEAD_INITIALIZER(client_head);
 
 #define PORT 9000
 #define BACKLOG 10
 #define DATA_FILE "/var/tmp/aesdsocketdata"
 
-volatile sig_atomic_t keep_running = 1;
-int fd_file = -1;
 int server_fd = -1;
+int data_fd = -1;
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+volatile sig_atomic_t running = 1;
 
-void cleanup()
-{
+// Cleanup resources
+void cleanup() {
     syslog(LOG_INFO, "Shutting down server ...");
-    if (fd_file >= 0) {
-        close(fd_file);
-    }
-    if (server_fd >= 0) {
-        close(server_fd);
-    }
+    if (server_fd >= 0) close(server_fd);
+    if (data_fd >= 0) close(data_fd);
     unlink(DATA_FILE);
     closelog();
 }
 
-void signal_handler(int sig) 
-{
-    keep_running = 0;
-    cleanup();
-    exit(0);
+// Signal handler
+void handle_signal(int sig) {
+    running = 0;
+    shutdown(server_fd, SHUT_RDWR);
 }
 
-int main(int argc, char* argv[]) 
-{
-    int client_fd;
-    struct sockaddr_in server_addr, client_addr;
-    socklen_t client_len = sizeof(client_addr);
+// Thread handler
+void* client_handler(void* arg) {
+    struct client* client = (struct client*)arg;
     char buffer[1024];
-    int daemon_mode = 0;
-    int daemon_opt;
+    ssize_t rcv_len;
+    bool packet_done = false;
 
-    while ((daemon_opt = getopt(argc, argv, "d")) != -1) {
-        switch (daemon_opt) {
-            case 'd':
-                daemon_mode = 1;
-                break;
-            default:
-                fprintf(stderr, "Usage: %s [-d]\n", argv[0]);
-                break;
+    sleep(1);
+    syslog(LOG_INFO, "thread started: client_fd = %d", client->client_fd);
+
+    while ((rcv_len = recv(client->client_fd, buffer, sizeof(buffer), 0)) > 0) {
+        pthread_mutex_lock(&mutex);
+        if (write(data_fd, buffer, rcv_len) != rcv_len) {
+            syslog(LOG_ERR, "Write failed");
+        }
+        pthread_mutex_unlock(&mutex);
+        if (memchr(buffer, '\n', rcv_len)) {
+            packet_done = true;
+            break;
         }
     }
-    
-    // Setting up signal handlers for SIGINT and SIGTERM
-    signal(SIGINT, signal_handler);
-    signal(SIGTERM, signal_handler);
 
-    // Open syslog
-    openlog("aesdsocket", LOG_PID, LOG_DAEMON);
+    if (rcv_len < 0) {
+        syslog(LOG_ERR, "Receive failed");
+    }
 
-    // Create socket
+    if (packet_done) {
+        pthread_mutex_lock(&mutex);
+        lseek(data_fd, 0, SEEK_SET);
+        while ((rcv_len = read(data_fd, buffer, sizeof(buffer))) > 0) {
+            if (send(client->client_fd, buffer, rcv_len, 0) != rcv_len) {
+                syslog(LOG_ERR, "Send failed");
+                break;
+            }
+        }
+        pthread_mutex_unlock(&mutex);
+    }
+
+    close(client->client_fd);
+    syslog(LOG_INFO, "Close connection on fd %d", client->client_fd);
+    client->complete = true;
+    pthread_exit(NULL);
+}
+
+int main(int argc, char* argv[]) {
+    int daemon_mode = 0;
+    int opt;
+
+    while ((opt = getopt(argc, argv, "d")) != -1) {
+        if (opt == 'd') daemon_mode = 1;
+    }
+
+    openlog("aesdsocket", LOG_PID, LOG_USER);
+    signal(SIGINT, handle_signal);
+    signal(SIGTERM, handle_signal);
+
+    struct sockaddr_in srv_addr = {0};
     server_fd = socket(AF_INET, SOCK_STREAM, 0);
     if (server_fd < 0) {
-        syslog(LOG_ERR, "Socket creation failed");
+        syslog(LOG_ERR, "Socket failed");
         cleanup();
-        return -1;
+        return EXIT_FAILURE;
     }
 
-    // Set socket options
-    int opt = 1;
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) < 0) {
-        syslog(LOG_ERR, "Setsockopt failed");
-        cleanup();
-        return -1;
-    }
+    int reuse = 1;
+    setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse));
 
-    // Configure server address
-    memset(&server_addr, 0, sizeof(server_addr));
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = INADDR_ANY;
-    server_addr.sin_port = htons(PORT);
+    srv_addr.sin_family = AF_INET;
+    srv_addr.sin_addr.s_addr = INADDR_ANY;
+    srv_addr.sin_port = htons(PORT);
 
-    // Bind socket
-    if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+    if (bind(server_fd, (struct sockaddr*)&srv_addr, sizeof(srv_addr)) < 0) {
         syslog(LOG_ERR, "Bind failed");
         cleanup();
-        return -1;
+        return EXIT_FAILURE;
     }
 
-    // Fork to daemonize if -d is specified
     if (daemon_mode) {
         pid_t pid = fork();
         if (pid < 0) {
             syslog(LOG_ERR, "Fork failed");
             cleanup();
-            return -1;
+            return EXIT_FAILURE;
         }
         if (pid > 0) {
-            // Exit parent process
-            cleanup();
-            return 0;
+            exit(EXIT_SUCCESS);
         }
 
-        // Continue child process
         umask(0);
-        if (setsid() < 0) {
-            syslog(LOG_ERR, "setsid failed");
-            cleanup();
-            return -1;
-        }
-        if (chdir("/") < 0) {
-            syslog(LOG_ERR, "chdir failed");
-            cleanup();
-            return -1;
-        }
+        setsid();
+        chdir("/");
         close(STDIN_FILENO);
         close(STDOUT_FILENO);
         close(STDERR_FILENO);
     }
 
-    // Listen for connections
     if (listen(server_fd, BACKLOG) < 0) {
         syslog(LOG_ERR, "Listen failed");
         cleanup();
-        return -1;
+        return EXIT_FAILURE;
     }
 
-    // Open or create data file
-    fd_file = open(DATA_FILE, O_RDWR | O_CREAT | O_APPEND, 0644);
-    if (fd_file < 0) {
+    data_fd = open(DATA_FILE, O_RDWR | O_CREAT | O_APPEND, 0644);
+    if (data_fd < 0) {
         syslog(LOG_ERR, "Failed to open data file");
         cleanup();
-        return -1;
+        return EXIT_FAILURE;
     }
 
-    while (keep_running) {
-        // Accept connection
-        client_fd = accept(server_fd, (struct sockaddr *)&client_addr, &client_len);
+    while (running) {
+        struct sockaddr_in cli_addr;
+        socklen_t cli_len = sizeof(cli_addr);
+        int client_fd = accept(server_fd, (struct sockaddr*)&cli_addr, &cli_len);
+
         if (client_fd < 0) {
-            if (keep_running) {
-                syslog(LOG_ERR, "Accept failed");
-            }
+            if (running) syslog(LOG_ERR, "Accept failed");
             continue;
         }
 
-        // Log connection
+        struct client* new_client = calloc(1, sizeof(struct client));
+        new_client->client_fd = client_fd;
+        new_client->complete = false;
+        SLIST_INSERT_HEAD(&client_head, new_client, entries);
+
+        if (pthread_create(&new_client->thread, NULL, client_handler, new_client) != 0) {
+            syslog(LOG_ERR, "Thread creation failed");
+            close(client_fd);
+            SLIST_REMOVE(&client_head, new_client, client, entries);
+            free(new_client);
+            continue;
+        }
+
         syslog(LOG_INFO, "Accepted connection from port %d", PORT);
-
-        // Receive and write data
-        ssize_t bytes_read;
-        int packet_complete = 0;
-        while ((bytes_read = recv(client_fd, buffer, sizeof(buffer) - 1, 0)) > 0) {
-            buffer[bytes_read] = '\0';
-            if (write(fd_file, buffer, bytes_read) != bytes_read) {
-                syslog(LOG_ERR, "Write to file failed");
-            }
-            
-            // Check for packet completion (newline)
-            if (strchr(buffer, '\n')) {
-                packet_complete = 1;
-                break;
-            }
-        }
-
-        if (bytes_read < 0 && keep_running) {
-            syslog(LOG_ERR, "Receive failed");
-        }
-
-        // Send file contents back to client if packet is complete
-        if (packet_complete) {
-            lseek(fd_file, 0, SEEK_SET);
-            while ((bytes_read = read(fd_file, buffer, sizeof(buffer) - 1)) > 0) {
-                if (send(client_fd, buffer, bytes_read, 0) != bytes_read) {
-                    syslog(LOG_ERR, "Send failed");
-                    break;
-                }
-            }
-        }
-
-        // Close client connection and log
-        close(client_fd);
-        syslog(LOG_INFO, "Closed connection from port %d", PORT);
     }
 
-    // Cleanup
+    // Join and free all threads
+    struct client *cp;
+    SLIST_FOREACH(cp, &client_head, entries) {
+        pthread_join(cp->thread, NULL);
+        SLIST_REMOVE(&client_head, cp, client, entries);
+        free(cp);
+    }
+
     cleanup();
-    return 0;
+    return EXIT_SUCCESS;
 }
